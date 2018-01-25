d0024af779f1ac8374620d4701b9ffb1c69c4066
ColinBolduc <300922@shakopeeschools.org>
2018-01-25 13:05:09 -0600

    TESTTESTTESTTEST
    TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
    TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST

--- example8.py
+++ example8.py
@@ -4,6 +4,7 @@
 #     strategy_name: a string
 #     strategy_description: a string
 #     move: A function that returns 'c' or 'b'
+#     TESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTESTTEST
 ####
 
 team_name = 'Forks'
